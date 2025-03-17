aa1d61e6710bef3d861a25c7f69533585357c8da
Héctor Chocobar-Torrejón <45291267+hchocobar@users.noreply.github.com>
2025-03-17 18:27:24 +0000

    [review] fix: code style according best practices

--- src/front/js/component/Navbar.jsx
+++ src/front/js/component/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light">
@@ -9,8 +10,8 @@ export const Navbar = () => {
 					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
 				</Link>
 				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+					<Link to="/">
+						<button className="btn btn-primary">button link</button>
 					</Link>
 				</div>
 			</div>
