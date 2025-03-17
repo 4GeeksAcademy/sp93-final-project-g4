0059f7e37c06d715df7e3e501bc25ea92dd6ce60
MauroG07 <71202727+MauroG07@users.noreply.github.com>
2025-03-17 20:17:05 +0000

    Models CinmeaRooms, ShowTimes, Movies DONE

--- src/api/models.py
+++ src/api/models.py
@@ -43,3 +43,68 @@ class Bookings(db.Model):
                 'showtime_id': self.showtime_id,
                 'col': self.col,
                 'row': self.row}
+
+class CinemaRooms(db.Model):
+    __tablename__ = "cinema_rooms"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    capacity = db.Column(db.Integer, nullable=False)
+    cinema_col = db.Column(db.Integer, default=5)
+    cinema_row = db.Column(db.Integer, default=5)
+
+    def __repr__(self):
+        return f'<Cinema Rooms: Name:{self.name} - {self.id}'
+    
+    def serialize(self):
+        return{ 'id': self.id,
+                'name': self.name,
+                'capacity': self.capacity,
+                'cinema_col': self.cinema_col,
+                'cinema_row': self.cinema_row}
+
+
+class ShowTimes(db.Model):
+    __tablename__ = "show_times"
+    id = db.Column(db.Integer, primary_key=True)
+    date_time = db.Column(db.Datetime)
+    movie_id = db.Column(db.Integer)
+    cinema_room_id = db.Column(db.Integer)
+    available = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'<Show Time: date time: {self.date_time} - movie : {self.movie_id}'
+    
+    def serialize(self):
+        return{ 'id': self.id,
+                'date_time': self.date_time,
+                'movie_id': self.movie_id,
+                'cinema_room_id': self.cinema_room_id,
+                'available': self.available}
+    
+
+class Movies(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tmdb_id = db.Column(db.Integer, unique=True)
+    title = db.Column(db.String, nullable=False)
+    duration = db.Column(db.Integer, nullalbe=False)
+    overview = db.Column(db.String)
+    adult = db.Column(db.Boolean)
+    backdrop_path = db.Column(db.String)
+    popularity = db.Column(db.Double)
+    poster_path = db.Column(db.String)
+    release_date = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<Movies: title{self.title}'
+    
+    def serialize(self):
+        return{ 'id': self.id,
+                'tmdb_id': self.tmdb_id,
+                'duration': self.duration,
+                'overview': self.overview,
+                'adult': self.adult,
+                'backdrop_path': self.backdrop_path,
+                'popularity': self.popularity,
+                'poster_path': self.poster_path,
+                'release_date': self.release_date}
+
